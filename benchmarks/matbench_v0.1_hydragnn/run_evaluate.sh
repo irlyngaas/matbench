93460da3e266e1f147cafcf90155f244f60c52b0
Isaac <irlyngaas@github.com>
2026-07-13 13:07:44 -0400

    Modify run script to add both tasks

--- benchmarks/matbench_v0.1_hydragnn/run_evaluate.sh
+++ benchmarks/matbench_v0.1_hydragnn/run_evaluate.sh
@@ -1,8 +1,6 @@
 tasks=""
-#for task in "mp_is_metal"; do
-#for task in "jdft2d" "mp_is_metal"; do
-for task in "jdft2d"; do
-  #bash preprocess_matbench.sh $task
+for task in "jdft2d" "mp_is_metal"; do
+  bash preprocess_matbench.sh $task
   bash evaluate_our_finetune.sh $task
   tasks+=" matbench_$task"
 done
--- benchmarks/matbench_v0.1_hydragnn/run_full.sh
+++ benchmarks/matbench_v0.1_hydragnn/run_full.sh
@@ -2,8 +2,7 @@ epochs=(100 10)
 
 num=0
 tasks=""
-#for task in "jdft2d" "mp_is_metal"; do
-for task in "jdft2d"; do
+for task in "jdft2d" "mp_is_metal"; do
   bash preprocess_matbench.sh $task
   bash finetune.sh $task ${epochs[num]}
   let num++
